79d1237ead579cd67d5569b57f43dec4a536015f
Cheng Li @ SenseTime <chengli.thu@gmail.com>
2024-02-10 10:00:35 +0800

    add base to float

--- ChatHaruhi/utils.py
+++ ChatHaruhi/utils.py
@@ -16,3 +16,30 @@ def base64_to_string(base64_data):
     text = byte_array.decode('utf-8')
     return text
 
+
+def float_array_to_base64(float_arr):
+    import base64
+    byte_array = b''
+    
+    for f in float_arr:
+        # 将每个浮点数打包为4字节
+        num_bytes = struct.pack('!f', f)  
+        byte_array += num_bytes
+    
+    # 将字节数组进行base64编码    
+    base64_data = base64.b64encode(byte_array)
+    
+    return base64_data.decode('utf-8')
+
+def base64_to_float_array(base64_data):
+    import base64
+    byte_array = base64.b64decode(base64_data)
+    
+    float_array = []
+    
+    # 每 4 个字节解析为一个浮点数
+    for i in range(0, len(byte_array), 4):
+        num = struct.unpack('!f', byte_array[i:i+4])[0] 
+        float_array.append(num)
+
+    return float_array
