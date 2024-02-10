4969173286cf5dca25a49ac44cf689f3f46e6493
Cheng Li @ SenseTime <chengli.thu@gmail.com>
2024-02-10 09:24:39 +0800

    Create embeddings.py
    增加foo embedding函数

---
+++ ChatHaruhi/embeddings.py
@@ -0,0 +1,7 @@
+import random
+
+def foo_embedding( text ):
+    # whatever text input , output a 2 dim 0-1 random vects
+    return [random.random(), random.random()]
+
+# TODO: add bge-zh-small(or family) and bce embedding here 米唯实
