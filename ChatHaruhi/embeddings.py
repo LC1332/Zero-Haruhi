528fe7980b512f32d5cec4daa11c73b09602563f
Cheng Li @ SenseTime <chengli.thu@gmail.com>
2024-02-10 09:44:27 +0800

    Update embeddings.py
    增加三个foo函数

--- ChatHaruhi/embeddings.py
+++ ChatHaruhi/embeddings.py
@@ -1,7 +1,25 @@
 import random
 
+# 这是用来调试的foo embedding
+
 def foo_embedding( text ):
     # whatever text input , output a 2 dim 0-1 random vects
     return [random.random(), random.random()]
+
+def foo_bge_zh_15( text ):
+    dim = 512
+    print("warning! foo_bge_zh_15 is not implemented yet, 请催促 米唯实 进行实现" )
+    return [random.random() for _ in range(dim)]
+
+def foo_bce( text )
+    dim = 768
+    print("warning! foo_bce is not implemented yet, 请催促 米唯实 进行实现" )
+    return [random.random() for _ in range(dim)]
+
+def foo_openai( text ):
+    dim = 1536
+    print("warning! foo_openai is not implemented yet, 请催促 米唯实 进行实现" )
+    return [random.random() for _ in range(dim)]
+
 
 # TODO: add bge-zh-small(or family) and bce embedding here 米唯实
