f70249e1bd3f149b62db3cde367180532cc6e273
King guys <68528929+2404589803@users.noreply.github.com>
2024-02-12 23:06:49 +0800

    update file

--- ChatHaruhi/response_zhipu.py
+++ ChatHaruhi/response_zhipu.py
@@ -12,24 +12,23 @@ def init_client():
     # 将client设置为全局变量
     global client
     
-    # 将ZhipuAI_API_KEY作为参数值传递给OS
-    api_key = os.environ.get("ZhipuAI_API_KEY")
+    # 将ZHIPUAI_API_KEY作为参数值传递给OS
+    api_key = os.environ("ZHIPUAI_API_KEY")
     if api_key is None:
-        raise ValueError("环境变量'ZhipuAI_API_KEY'未设置，请确保已经定义了API密钥")
+        raise ValueError("环境变量'ZHIPUAI_API_KEY'未设置，请确保已经定义了API密钥")
 
 def init_aclient():
     
     # 将aclient设置为全局变量
     global aclient
 
-    # 将ZhipuAI_API_KEY作为参数值传递给OS
-    api_key = os.environ.get("ZhipuAI_API_KEY")
+    # 将ZHIPUAI_API_KEY作为参数值传递给OS
+    api_key = os.environ("ZHIPUAI_API_KEY")
     if api_key is None:
-        raise ValueError("环境变量'ZhipuAI_API_KEY'未设置，请确保已经定义了API密钥")
+        raise ValueError("环境变量'ZHIPUAI_API_KEY'未设置，请确保已经定义了API密钥")
     
     # 异步调用
 
-    async def async_get_response()
 
 
 
