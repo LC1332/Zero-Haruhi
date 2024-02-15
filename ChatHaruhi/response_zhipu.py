3921f6afc6482d6620fad8cb9e11bf37991487ae
Cheng Li @ SenseTime <chengli.thu@gmail.com>
2024-02-15 09:49:54 +0800

    fix environ -> getenv in zhipuai

--- ChatHaruhi/response_zhipu.py
+++ ChatHaruhi/response_zhipu.py
@@ -13,7 +13,7 @@ def init_client():
     global client
     
     # 将ZHIPUAI_API_KEY作为参数值传递给OS
-    api_key = os.environ("ZHIPUAI_API_KEY")
+    api_key = os.getenv("ZHIPUAI_API_KEY")
     if api_key is None:
         raise ValueError("环境变量'ZHIPUAI_API_KEY'未设置，请确保已经定义了API密钥")
 
@@ -24,7 +24,7 @@ def init_aclient():
     global aclient
 
     # 将ZHIPUAI_API_KEY作为参数值传递给OS
-    api_key = os.environ("ZHIPUAI_API_KEY")
+    api_key = os.getenv("ZHIPUAI_API_KEY")
     if api_key is None:
         raise ValueError("环境变量'ZHIPUAI_API_KEY'未设置，请确保已经定义了API密钥")
 
