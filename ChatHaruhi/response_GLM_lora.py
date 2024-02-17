478337938ddb560fb11adc1781780dbfb6f3a854
JiangYH <jiangyuhangjiangyuhang@outlook.com>
2024-02-17 13:28:37 +0000

    glm lora 推理

--- ChatHaruhi/response_GLM_lora.py
+++ ChatHaruhi/response_GLM_lora.py
@@ -115,7 +115,7 @@ def message2query(messages: List[Dict[str, str]]) -> str:
     return "".join([template.substitute(message) for message in messages])
 
 
-def get_response(message, model_name: str = "/workspace/jyh/Zero-Haruhi/lora", verbose: bool = True):
+def get_response(message, model_name: str = "silk-road/Haruhi-Zero-GLM3-6B-Lora-0_4", verbose: bool = True):
     global client
     global tokenizer
 
