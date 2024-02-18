e1e9f77e79816dfb4b2f0a4deafc740e5c90c555
JiangYH <jiangyuhangjiangyuhang@outlook.com>
2024-02-18 09:54:00 +0000

    glm full local

--- ChatHaruhi/response_GLM_local.py
+++ ChatHaruhi/response_GLM_local.py
@@ -95,7 +95,7 @@ def message2query(messages: List[Dict[str, str]]) -> str:
     return "".join([template.substitute(message) for message in messages])
 
 
-def get_response(message, model_name: str = "THUDM/chatglm3-6b", verbose: bool = True):
+def get_response(message, model_name: str = "THUDM/chatglm3-6b", verbose: bool = False):
     global client
     global tokenizer
 
